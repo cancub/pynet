726e49be99073191c934ba6a2e17e48e8bece549
alf <alf@okenney.ca>
2023-02-01 20:15:25 -0500

    Add initial docs

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyNet'
+copyright = "2023, Alf O'Kenney"
+author = "Alf O'Kenney"
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+.. PyNet documentation master file, created by
+   sphinx-quickstart on Thu Jan 26 21:23:36 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyNet's documentation!
+===================================
+
+**PyNet** is a Python library for building your very own computer networks from
+the ground up.  It uses the details of the `Open Systems Interconnection (OSI)
+model <https://en.wikipedia.org/wiki/OSI_model>`_ to simulate the behavior of
+the various media, hardware and software that make up a network, allowing users
+to simulate actions such as file transfers or even network attacks.
+
+.. note::
+
+   This project is under active development.
+
+
+Contents
+--------
+
+.. toctree::
+
+   usage
