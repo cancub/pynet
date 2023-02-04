0f6c70db16765750663325611ce608580b1eddbd
alf <alf@okenney.ca>
2023-02-03 22:31:25 -0500

    Flesh our behaviour of Medium and Transceiver base classes

--- src/pynet/physical/base.py
+++ src/pynet/physical/base.py
@@ -1,17 +1,84 @@
+from __future__ import annotations
+
 import logging
 from abc import ABC, abstractmethod
-from typing import Sequence
+from typing import Any, Sequence
 
 __all__ = ['Medium', 'Transceiver']
+
 log = logging.getLogger(__name__)
 
 
 class Medium(ABC):
-    pass
+    """A base class for all physical media.
+
+    Just as in real life, a :class:`Medium` is passive in its interactions with a
+    :class:`Transceiver`. That is, a :class:`Medium` instance does not initiate any
+    connections or disconnections, nor does it send or receive data. In PyNet, it exists
+    merely as a record-keeper of the :class:`Transceiver` instances that are connected to
+    it and as a means of transporting data between those :class:`Transceiver` instances in
+    a manner that simulates the physical medium.
+
+    Subclasses of :class:`Medium` define their own connection and disconnection logic by
+    overriding the :meth:`_subclass_connect` and :meth:`_subclass_disconnect` methods.
+    Examples of such logic may include registering :class:`Transceiver` instances as a
+    possible recipient of data or confirming that there is enough space on the physical
+    medium to accommodate a new connection (e.g., a coaxial cable to which 802.3 Ethernet
+    transceivers connect via vampire taps).
+
+    NOTE: Instances of :class:`Medium` subclasses must be interacted with via methods of
+    a :class:`Transceiver` instance. That is, if a :class:`Medium` instance is to be
+    connected to a :class:`Transceiver` instance, the :meth:`Transceiver.connect` method
+    must with the :class:`Medium` instance as an argument."""
+
+    _transceivers: dict[Transceiver, Any] = {}
+
+    def __repr__(self) -> str:
+        # This is just a placeholder to prevent logs from being unwieldy. Subclasses
+        # should override this method to provide more useful information.
+        return f'<{self.__class__.__name__}>'
+
+    @abstractmethod
+    def _subclass_connect(self, transceiver: Transceiver) -> Any:
+        """Run class-specific connection logic and return any metadata to be stored in
+        the :attr:`_transceivers` dictionary alongside the :class:`Transceiver`"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _subclass_disconnect(self, transceiver: Transceiver):
+        """Run class-specific disconnection logic associated with the removal of a
+        :class:`Transceiver` from the medium"""
+        raise NotImplementedError
+
+    def _connect(self, transceiver: Transceiver, **kwargs):
+        """Connect a :class:`Transceiver` instance to the medium and optionally store
+        metadata provided by the subclass.
+
+        :param transceiver: The :class:`Transceiver` instance to connect to the medium.
+
+        NOTE: This method is called by :meth:`Transceiver.connect` and must not be
+        called in any other contexts."""
+        self._transceivers[transceiver] = self._subclass_connect(transceiver, **kwargs)
+
+    def _disconnect(self, transceiver: Transceiver):
+        """Disconnect a :class:`Transceiver` instance from the medium.
+
+        :param transceiver: The :class:`Transceiver` instance to disconnect from the
+            medium.
+
+        NOTE: This method is called by :meth:`Transceiver.disconnect` and must not be
+        called in any other contexts."""
+        self._subclass_disconnect(transceiver)
+
+        del self._transceivers[transceiver]
 
 
 class Transceiver(ABC):
-    supported_media: Sequence[Medium]
+    """A base class for all transceivers. A transceiver is a device that can send and
+    receive data over a physical medium. In PyNet, a :class:`Transceiver` instance is
+    responsible for connecting to a :class:`Medium` instance."""
+
+    _supported_media: Sequence[Medium]
     _medium: Medium = None
 
     def __init_subclass__(cls, supported_media: Sequence[Medium]) -> None:
@@ -19,14 +86,37 @@ class Transceiver(ABC):
         if bad_media:
             raise TypeError(f'{bad_media} are not subclasses of {Medium}')
 
-        cls.supported_media = supported_media
-
-    def __init__(self, medium: Medium = None) -> None:
-        if medium:
-            self.connect(medium)
+        cls._supported_media = supported_media
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__} (medium={self._medium})'
+        # This is just a placeholder to prevent logs from being unwieldy. Subclasses
+        # should override this method to provide more useful information.
+        if self._medium:
+            medium_str = f'medium={self._medium.__class__.__name__}'
+        else:
+            medium_str = 'disconnected'
+        return f'<{self.__class__.__name__} ({medium_str})>'
+
+    @abstractmethod
+    def _connect(self, medium: Medium, **kwargs):
+        """Class-specific connection logic"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _disconnect(self):
+        """Class-specific disconnection logic"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def put(self, data: str):
+        """Send data over the medium"""
+        # TODO: data should probably be a bytes-like object
+        raise NotImplementedError
+
+    @abstractmethod
+    def listen(self, timeout: int | float = None):
+        """Receive data from the medium"""
+        raise NotImplementedError
 
     @property
     def medium(self) -> Medium:
@@ -36,62 +126,57 @@ class Transceiver(ABC):
     def medium(self, medium: Medium) -> None:
         self.connect(medium)
 
-    def connect(self, medium: Medium, replace=False) -> None:
-        # Connecting nothing is the same as disconnecting.
-        if not medium:
+    def connect(self, new_medium: Medium, replace=False, **kwargs) -> None:
+        if not new_medium:
+            # :meth:`Transceiver.connect` could have been called via the setter method
+            # with a `None` value. We want to let this happen without raising an error,
+            # but we should log the re-routing for debugging purposes.
+            log.debug('Connecting to non-existant medium. Assuming disconnect')
             self.disconnect()
-            return
-
-        if medium is self._medium:
-            # Already connected to the same medium
             return
 
         # Make sure to check the validity of the incoming medium prior to
         # (potentially) disconnecting from the current medium.
-        if medium.__class__ not in self.supported_media:
-            raise ValueError(f'Medium {medium} not supported by {self}')
+        if (medium_cls := new_medium.__class__) not in self._supported_media:
+            raise ValueError(f'Medium {medium_cls.__name__} not supported by {self!r}')
 
-        # Validate medium prior to connecting.
-        if self._medium is not None:
+        # If the :class:`Transceiver` is already connected to a :class:`Medium`. Nothing
+        # to do if this is the same medium being connected. Otherwise make sure to
+        # disconnect from the previous medium (or raise an error if ``replace``` is
+        # `False`).
+        if current_medium := self._medium:
+            if new_medium is current_medium:
+                log.debug(f'{self!r} already connected to {new_medium!r}')
+                return
+
             if replace:
-                # The caller has specified that they want to replace the current
-                # medium with the new one, which is fine. We just need to
-                # perform the proper disconnect logic first.
+                log.debug(f'Replacing {current_medium} with {new_medium!r}')
                 self.disconnect()
             else:
                 raise RuntimeError(
-                    f'{self} already connected to {self._medium}'
+                    f'{self!r} already connected to {current_medium!r}. Use replace=True '
+                    'to swap mediums or call disconnect() first'
                 )
 
-        log.debug(f'{self} connecting to {medium}')
+        log.debug(f'Connecting {self!r} to {new_medium!r}')
 
-        self._connect(medium)
-        self._medium = medium
-
-    def _connect(self, medium: Medium):
-        """Optional class-specific connection logic"""
-        pass
+        # Make sure that both the subclass and the :class:`Medium` subclass run their
+        # respective connection logic.
+        self._connect(new_medium, **kwargs)
+        new_medium._connect(self, **kwargs)
+        self._medium = new_medium
 
     def disconnect(self) -> None:
-        if not self._medium:
+        if not (medium := self._medium):
             # Already disconnected
             return
 
-        log.debug(f'{self} disconnecting from {self._medium}')
+        # No need to specify the medium here since :meth:`__repr__` will return the
+        # medium detatils.
+        log.debug(f'Disconnecting {self!r}')
+
+        # Make sure that both the subclass and the :class:`Medium` subclass run their
+        # respective disconnection logic.
         self._disconnect()
+        medium._disconnect(self)
         self._medium = None
-
-    def _disconnect(self):
-        """Optional class-specific disconnection logic"""
-        pass
-
-    @abstractmethod
-    def send(self, data: str):
-        """Send data over the medium"""
-        # TODO: data should probably be a bytes-like object
-        pass
-
-    @abstractmethod
-    def receive(self, timeout: int | float = None):
-        """Receive data from the medium"""
-        pass
--- tests/physical/test_base.py
+++ tests/physical/test_base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
+from contextlib import contextmanager
 from unittest import TestCase, main
 from unittest.mock import call, patch
 
@@ -8,15 +11,53 @@ from pynet.testing import LogTestingMixin
 BASE_TARGET = 'pynet.physical.base'
 
 
-class MockTransceiver(Transceiver, supported_media=[Medium]):
-    def send(self, data: str):
+class MockMedium(Medium):
+    """A helper class for testing the :class:`Medium` class. Defines the necessary
+    abstract methods to instantiate the class."""
+
+    def __init__(self, name: str = 'mock'):
+        super().__init__()
+        # A name is useful for test debugging.
+        self.name = name
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} (name={self.name!r})>'
+
+    def _subclass_connect(self, transceiver: MockTransceiver):
         pass
 
-    def receive(self, timeout: int | float = None):
+    def _subclass_disconnect(self, transceiver: MockTransceiver):
+        pass
+
+
+class MockTransceiver(Transceiver, supported_media=[MockMedium]):
+    """A helper class for testing the :class:`Transceiver` class. Defines the necessary
+    abstract methods to instantiate the class."""
+
+    def _connect(self, medium: MockMedium):
+        pass
+
+    def _disconnect(self):
+        pass
+
+    def put(self, data: str):
+        pass
+
+    def listen(self, timeout: int | float = None):
         pass
 
 
 class TestTransceiver(TestCase, LogTestingMixin):
+    @contextmanager
+    def assertInBaseLogs(self, level, msgs, *args, **kwargs):
+        with self.assertInLogs(BASE_TARGET, level, msgs, *args, **kwargs):
+            yield
+
+    @contextmanager
+    def assertNotInBaseLogs(self, level, msgs, *args, **kwargs):
+        with self.assertNotInLogs(BASE_TARGET, level, msgs, *args, **kwargs):
+            yield
+
     def test__init_subclass__with_bad_media(self):
         with self.assertRaises(TypeError):
 
@@ -29,31 +70,18 @@ class TestTransceiver(TestCase, LogTestingMixin):
         class GoodTransceiver(Transceiver, supported_media=test_media):
             pass
 
-        self.assertEqual(GoodTransceiver.supported_media, test_media)
-
-    @patch.object(MockTransceiver, 'connect')
-    def test_init_with_and_without_medium(self, connect_mock):
-        for medium in (None, Medium()):
-            with self.subTest(medium=medium):
-                MockTransceiver(medium=medium)
-
-                if medium:
-                    connect_mock.assert_called_once_with(medium)
-                else:
-                    connect_mock.assert_not_called()
-
-            connect_mock.reset_mock()
+        self.assertEqual(GoodTransceiver._supported_media, test_media)
 
     @patch.object(MockTransceiver, 'connect')
     def test_medium_setter_wraps_connect(self, connect_mock):
         transceiver = MockTransceiver()
-        medium = Medium()
+        medium = MockMedium()
         transceiver.medium = medium
 
         connect_mock.assert_called_once_with(medium)
 
     def test_medium_getter(self):
-        medium = Medium()
+        medium = MockMedium()
         transceiver = MockTransceiver()
         self.assertIsNone(transceiver.medium)
 
@@ -62,30 +90,42 @@ class TestTransceiver(TestCase, LogTestingMixin):
 
     @patch.object(MockTransceiver, 'disconnect')
     def test_connecting_nothing_same_as_disconnecting(self, disconnect_mock):
-        transceiver = MockTransceiver(medium=Medium())
-        transceiver.connect(None)
+        transceiver = MockTransceiver()
+        transceiver.medium = MockMedium()
+
+        with self.assertInBaseLogs(
+            'DEBUG', 'Connecting to non-existant medium. Assuming disconnect'
+        ):
+            transceiver.connect(None)
 
         disconnect_mock.assert_called_once()
 
     def test_connecting_same_medium_does_nothing(self):
-        medium = Medium()
-        transceiver = MockTransceiver(medium=medium)
+        medium_name = 'test'
+        medium = MockMedium(name=medium_name)
+        transceiver = MockTransceiver()
+        transceiver.medium = medium
 
-        with self.assertNotInLogs(BASE_TARGET, 'DEBUG', 'connecting to'):
+        with self.assertNotInBaseLogs(
+            'DEBUG',
+            'Connecting.*MockTransceiver',
+            regex=True,
+        ):
             transceiver.connect(medium)
 
     def test_connect_unsupported_medium_raises(self):
         transceiver = MockTransceiver()
 
-        with self.assertRaisesRegex(ValueError, 'not supported'):
+        with self.assertRaisesRegex(ValueError, 'Medium.*not supported by.*Transceiver'):
             transceiver.connect(object())
 
     def test_connecting_without_replace_when_already_connected_raises(self):
-        medium = Medium()
-        transceiver = MockTransceiver(medium=medium)
+        medium = MockMedium()
+        transceiver = MockTransceiver()
+        transceiver.medium = medium
 
-        with self.assertRaisesRegex(RuntimeError, 'already connected to'):
-            transceiver.connect(Medium())
+        with self.assertRaisesRegex(RuntimeError, 'Transceiver.*already connected to'):
+            transceiver.connect(MockMedium())
 
     @patch.object(MockTransceiver, '_disconnect')
     @patch.object(MockTransceiver, '_connect')
@@ -93,60 +133,91 @@ class TestTransceiver(TestCase, LogTestingMixin):
         self, connect_mock, disconnect_mock
     ):
         # This actually tests both the connect and disconnect logic.
-        first_medium = Medium()
-        second_medium = Medium()
+        first_name = 'first'
+        second_name = 'second'
+        first_medium = MockMedium(name=first_name)
+        second_medium = MockMedium(name=second_name)
+        transceiver = MockTransceiver()
 
-        expected_logs = [
-            f'connecting to {first_medium}',
-            'disconnecting from',
-            f'connecting to {second_medium}',
-        ]
-        with self.assertInLogs(BASE_TARGET, 'DEBUG', expected_logs):
-            transceiver = MockTransceiver(medium=first_medium)
+        disconnected_str = '<MockTransceiver (disconnected)>'
+
+        with self.assertInBaseLogs(
+            'DEBUG',
+            f'Connecting {disconnected_str} to <MockMedium (name={first_name!r})>',
+        ):
+            transceiver.connect(first_medium)
+
+        with self.assertInBaseLogs(
+            'DEBUG',
+            [
+                'Disconnecting <MockTransceiver (medium=MockMedium)>',
+                f'Connecting {disconnected_str} to <MockMedium (name={second_name!r})>',
+            ],
+        ):
             transceiver.connect(second_medium, replace=True)
 
-        connect_mock.assert_has_calls(
-            [call(first_medium), call(second_medium)]
-        )
+        connect_mock.assert_has_calls([call(first_medium), call(second_medium)])
         disconnect_mock.assert_called_once()
         self.assertEqual(transceiver._medium, second_medium)
 
     @patch.object(MockTransceiver, 'disconnect')
-    def test_connecting_when_disconnected_does_not_disconnect(
-        self, disconnect_mock
-    ):
+    def test_connecting_when_disconnected_does_not_disconnect(self, disconnect_mock):
         transceiver = MockTransceiver()
-        medium = Medium()
+        medium = MockMedium()
 
-        with self.assertNotInLogs(BASE_TARGET, 'DEBUG', 'disconnecting from'):
-            with self.assertInLogs(
-                BASE_TARGET, 'DEBUG', f'connecting to {medium}'
+        with self.assertNotInBaseLogs(
+            'DEBUG', 'Disconnecting.*MockTransceiver', regex=True
+        ):
+            with self.assertInBaseLogs(
+                'DEBUG',
+                'Connecting.*MockTransceiver.*to.*MockMedium',
+                regex=True,
             ):
                 transceiver.connect(medium)
 
         disconnect_mock.assert_not_called()
 
     @patch.object(MockTransceiver, '_disconnect')
-    def test_disconnect_called_when_already_disconnected(
-        self, disconnect_mock
-    ):
+    def test_disconnect_called_when_already_disconnected(self, disconnect_mock):
         transceiver = MockTransceiver()
 
-        with self.assertNotInLogs(BASE_TARGET, 'DEBUG', 'disconnecting from'):
+        with self.assertNotInBaseLogs(
+            'DEBUG', 'Disconnecting.*MockTransceiver', regex=True
+        ):
             transceiver.disconnect()
 
         disconnect_mock.assert_not_called()
 
     def test_successful_disconnect(self):
-        medium = Medium()
-        transceiver = MockTransceiver(medium=medium)
+        medium = MockMedium()
+        transceiver = MockTransceiver()
+        transceiver.medium = medium
 
-        with self.assertInLogs(
-            BASE_TARGET, 'DEBUG', f'disconnecting from {medium}'
-        ):
+        with self.assertInBaseLogs('DEBUG', 'Disconnecting.*MockTransceiver', regex=True):
             transceiver.disconnect()
 
         self.assertIsNone(transceiver._medium)
+
+
+class TestMedium(TestCase, LogTestingMixin):
+    @patch.object(MockMedium, '_subclass_disconnect')
+    @patch.object(MockMedium, '_subclass_connect')
+    def test_metadata_processing_in_connect_and_disconnect(
+        self, connect_mock, disconnect_mock
+    ):
+        medium = MockMedium()
+        transceiver = MockTransceiver()
+
+        connect_mock.return_value = metadata = {'test': 'value'}
+
+        transceiver.connect(medium)
+
+        connect_mock.assert_called_once_with(transceiver)
+        self.assertEqual(metadata, medium._transceivers[transceiver])
+
+        transceiver.disconnect()
+        disconnect_mock.assert_called_once_with(transceiver)
+        self.assertNotIn(transceiver, medium._transceivers)
 
 
 if __name__ == '__main__':
