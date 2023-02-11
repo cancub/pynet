e84634e322baa851cab72e00d3444592af5515a4
alf <alf@okenney.ca>
2023-02-10 22:50:16 -0500

    Add multiprocessing and necessary components for IPC between transceivers over medium

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
 import logging
+from itertools import product
 from unittest import TestCase, main
+from unittest.mock import Mock, patch
 from contextlib import ExitStack
 
-from pynet.testing import LogTestingMixin
+from pynet.physical.base import Medium, Transceiver
+from pynet.testing import LogTestingMixin, ProcessBuilderMixin
 
 log = logging.getLogger(__name__)
+
+TESTING_TARGET = 'pynet.testing'
 
 
 class TestLogTestingMixin(TestCase):
@@ -82,6 +89,139 @@ class TestLogTestingMixin(TestCase):
         with self.assertRaises(AssertionError):
             with self.mixin_tester.assertInLogs(__name__, logging.DEBUG, 'test'):
                 pass
+
+
+class MockMedium(Medium, dimensionality=1):
+    """A helper class for testing the :class:`TestProcessBuilderMixin` mixin."""
+
+    def _connect(self, args, **kwargs):
+        pass
+
+    def _disconnect(self, args, **kwargs):
+        pass
+
+    def _process_transmission(self, args, **kwargs):
+        pass
+
+
+class MockTransceiver(Transceiver, supported_media=[MockMedium]):
+    """A helper class for testing the :class:`TestProcessBuilderMixin` mixin."""
+
+    location: int = None
+
+    def _connect(self, args, **kwargs):
+        pass
+
+    def _disconnect(self, args, **kwargs):
+        pass
+
+    def _process_incoming_data(self, args, **kwargs):
+        pass
+
+    def _process_outgoing_data(self, args, **kwargs):
+        pass
+
+
+class TestProcessBuilderMixin(TestCase):
+    @patch.object(MockMedium, 'start')
+    def test_build_medium(self, start_mock):
+        class MixinTester(TestCase, ProcessBuilderMixin):
+            medium_cls = MockMedium
+
+        self.medium_tester = MixinTester()
+
+        test_specs = product(
+            # name
+            (None, 'test'),
+            # mocked
+            (True, False),
+            # is_alive
+            (None, True, False),
+            # auto_start
+            (True, False),
+        )
+
+        for name, mocked, is_alive, auto_start in test_specs:
+            auto_gen_name = f'test_{len(Medium._instances)}'
+
+            medium = self.medium_tester.build_medium(
+                name=name, mocked=mocked, is_alive=is_alive, auto_start=auto_start
+            )
+
+            with self.subTest(
+                name=name, mocked=mocked, is_alive=is_alive, auto_start=auto_start
+            ):
+                self.assertIsInstance(medium, Medium)
+
+                # Checks dependent on `mocked`
+                self.assertEqual(mocked, isinstance(medium, Mock))
+
+                if not mocked and auto_start:
+                    start_mock.assert_called_once()
+                else:
+                    start_mock.assert_not_called()
+
+                self.assertEqual(mocked, isinstance(medium._connection_queue, Mock))
+
+                # Checks independent of `mocked`
+                if name is not None:
+                    self.assertEqual(name, medium.name)
+                else:
+                    self.assertEqual(auto_gen_name, medium.name)
+
+                if is_alive is not None:
+                    self.assertEqual(is_alive, medium.is_alive())
+
+            start_mock.reset_mock()
+
+    @patch.object(MockTransceiver, 'start')
+    def test_build_xcvr(self, start_mock):
+        class MixinTester(TestCase, ProcessBuilderMixin):
+            xcvr_cls = MockTransceiver
+
+        self.xcvr_tester = MixinTester()
+
+        test_specs = product(
+            # name
+            (None, 'test'),
+            # mocked
+            (True, False),
+            # is_alive
+            (None, True, False),
+            # auto_start
+            (True, False),
+        )
+
+        for name, mocked, is_alive, auto_start in test_specs:
+            auto_gen_name = f'test_{len(Transceiver._instances)}'
+
+            xcvr = self.xcvr_tester.build_xcvr(
+                name=name, mocked=mocked, is_alive=is_alive, auto_start=auto_start
+            )
+
+            with self.subTest(
+                name=name, mocked=mocked, is_alive=is_alive, auto_start=auto_start
+            ):
+                self.assertIsInstance(xcvr, Transceiver)
+
+                # Checks dependent on `mocked`
+                self.assertEqual(mocked, isinstance(xcvr, Mock))
+
+                if not mocked and auto_start:
+                    start_mock.assert_called_once()
+                else:
+                    start_mock.assert_not_called()
+
+                # Checks independent of `mocked`
+                if name is not None:
+                    self.assertEqual(name, xcvr.name)
+                else:
+                    self.assertEqual(auto_gen_name, xcvr.name)
+
+                if is_alive is not None:
+                    self.assertEqual(is_alive, xcvr.is_alive())
+
+            start_mock.reset_mock()
 
 
 if __name__ == '__main__':
